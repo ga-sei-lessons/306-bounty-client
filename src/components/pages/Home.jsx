eb2ba5fa4ea6d561097832ac31e0d6c83dafd52b
weston-bailey <wdbailey87@gmail.com>
2023-04-28 14:40:34 -0700

    debug

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,23 +1,74 @@
 import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import BountyForm from "../partials/BountyForm"
 import axios from "axios"
 
 export default function Home() {
     // state to hold the data payload from our backend
     const [bounties, setBounties] = useState([])
+    // show/hide bounty creation form
+    const [showForm, setShowForm] = useState(false)
     // useEffect to get the data payload from our backend
     useEffect(() => {
         const url = `${process.env.REACT_APP_SERVER_URL}/bounties`
         axios.get(url)
             .then(response => {
-                console.log(response.data)
+                setBounties(response.data.results)
             })
             .catch(console.warn)
     }, [])
-    console.log(process.env.REACT_APP_SERVER_URL)
+    // console.log(process.env.REACT_APP_SERVER_URL)
+
+    const handleSubmit = (e, form) => {
+        e.preventDefault()
+        console.log("form has submitted!")
+        // POST a bounty
+    }
+
+    const handleCancelClick = () => setShowForm(false)
+
+    const initialState = {
+        name: "",
+        wantedFor: "",
+        client: "",
+        ship: "",
+        reward: 0,
+        captured: false,
+        lastSend: ""
+    }
+
+    const bountyListItems = bounties.map(bounty => {
+        return (
+            <li key={`bounty-li ${bounty._id}`}>
+                <Link to={`/bounties/${bounty._id}`}>
+                    {bounty.name} : ${bounty.reward}
+                </Link>
+            </li>
+        )
+    })
 
     return (
         <div>
-            home
+            <h1>Bounty App</h1>
+            <p> {bounties.length === 0 && "loading bounties..."} </p>
+
+            {/* short circuiting */}
+            {!showForm && <button
+                onClick={() => setShowForm(true)}
+            >
+                Show Bounty Creation Form
+            </button>}
+
+            {!showForm ?
+            <ul>
+                {bountyListItems}
+            </ul> :
+                <BountyForm 
+                    initialState={initialState}
+                    handleCancelClick={handleCancelClick}
+                    handleSubmit={handleSubmit}
+                />
+            }
         </div>
     )
 }
--- src/components/partials/BountyForm.jsx
+++ src/components/partials/BountyForm.jsx
@@ -1,7 +1,37 @@
-export default function BountyForm() {
+import { useState } from "react"
+
+export default function BountyForm(props) {
+/*
+    props for initial state
+    {
+        name: "",
+        wantedFor: "",
+        client: "",
+        ship: "",
+        reward: 0,
+        captured: false,
+        lastSend: ""
+    }
+*/
+
+    const [form, setForm] = useState(props.initialState)
     return (
         <div>
-            bounty form
+           <form onSubmit={e => props.handleSubmit(e, form)}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+
+                    <input 
+                        type="text"
+                        placeholder="enter name"
+                        id="name"
+                        value={form.name}
+                        onChange={e => setForm({ ...form, name: e.target.value })}
+                    />
+                </div>
+           </form>
+
+           <button onClick={props.handleCancelClick}>Cancel</button>
         </div>
     )
 }
