9dce3f50d1cf4c38b1da844dcff5c9023c0d3c4f
weston-bailey <wdbailey87@gmail.com>
2023-04-28 15:12:14 -0700

    adds bounty creation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -19,10 +19,26 @@ export default function Home() {
     }, [])
     // console.log(process.env.REACT_APP_SERVER_URL)
 
-    const handleSubmit = (e, form) => {
+    const handleSubmit = async (e, form) => {
         e.preventDefault()
         console.log("form has submitted!")
+        console.log(form)
         // POST a bounty
+        try {
+            // axios.post(url, request body, { options })
+            // post the new bounty to the backed
+            await axios.post(`${process.env.REACT_APP_SERVER_URL}/bounties`, form) // ignoring return value of the server's response
+            // if the response with success, we want to get request to see all
+            const allBounties = await axios.get(`${process.env.REACT_APP_SERVER_URL}/bounties`)
+            setBounties(allBounties.data.results) //update state
+            setShowForm(false) //close the form
+        } catch (err) {
+            // mongoose errors would wind up here
+            // display relevant errors to user if needed
+            // set error message state to display to the user
+            console.warn(err)
+        }
+
     }
 
     const handleCancelClick = () => setShowForm(false)
